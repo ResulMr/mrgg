923dbf2712121b9b0d0442b7efd3f183eeeddff1
Resul <faridgrandov33@gmail.com>
2024-10-18 21:15:57 +0400

    salam

---
+++ app.js
@@ -0,0 +1,29 @@
+/*1*/
+const h2Inner = document.getElementById('h2Inner')
+h2Inner.innerText = "Brawl Stars"
+ 
+/*2*/
+
+ 
+/*3*/
+function myFunctiion() {
+    document.body.style.backgroundColor = "green";
+  }
+
+/*7*/
+function myFunction() {
+  document.getElementById("rasul").style.visibility = "hidden";
+}
+
+/*8*/
+const red = document.getElementById('red').style.color = "red"
+const blue = document.getElementById('blue').style.color = "blue"
+const green = document.getElementById('green').style.color = "green"
+const yellow = document.getElementById('yellow').style.color = "yellow"
+
+/*9*/
+
+/*10*/
+
+
+
